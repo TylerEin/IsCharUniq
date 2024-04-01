71a20b7a02820086ff48c5818b6027693a55b3f1
Сергей Сергей <eintyler23rus@gmail.com>
2024-04-01 16:13:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Задача");
+Console.WriteLine("Написать функцию проверки строки на уникальность символов");
+Console.WriteLine("***********\nВведите данные");
+string s = Console.ReadLine();
